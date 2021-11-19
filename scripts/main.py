5d01d6525584fcea40a12f5c7b13321b3b504cda
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-11-20 01:59:28 +0900

    Fix

--- scripts/main.py
+++ scripts/main.py
@@ -5,26 +5,24 @@ import feedparser
 RSS_URL = 'https://kinpokoblog.com/index.xml'
 GENERATE_FILE_NAME = 'README.md'
 
-def main():
-    
 
-    # ブログの新着記事以外の部分を書き込む
+def main():
+
+    # ブログの新着記事以外
     docs = textwrap.dedent("""\
 
 
     # Hi!
-    
+
     [![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=kinpoko)](https://github.com/anuraghazra/github-readme-stats)
-    
-    [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=kinpoko&langs_count=8&layout=compact)](https://github.com/anuraghazra/github-readme-stats)
-    
+
+    [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=kinpoko&langs_count=4)](https://github.com/anuraghazra/github-readme-stats)
+
     ## Recent Posts on [My blog](https://kinpokoblog.com)
     """)
 
-
     with open(GENERATE_FILE_NAME, 'w') as f:
         f.write(docs)
-
 
     # ブログの新着記事を書き込む
     d = feedparser.parse(RSS_URL)
@@ -32,14 +30,12 @@ def main():
         docs = textwrap.dedent("""\
             - [{title}]({url})
             """).format(
-                title = entry.title,
-                url = entry.link
-            )
+            title=entry.title,
+            url=entry.link
+        )
 
         with open(GENERATE_FILE_NAME, 'a') as f:
             f.write(docs)
-
-
 
 
 if __name__ == '__main__':
