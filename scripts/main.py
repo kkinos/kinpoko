37852f585f9a2b3a64d56e713da5e9138675fbd3
kinpoko <79001545+kinpoko@users.noreply.github.com>
2022-05-18 22:33:10 +0900

    Change script

--- scripts/main.py
+++ scripts/main.py
@@ -12,12 +12,13 @@ def main():
     docs = textwrap.dedent("""\
 
 
-    # Hi there :wave:
+    ## Hi there :wave:
 
-   [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=kinpoko)](https://github.com/anuraghazra/github-readme-stats)
-   [![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=kinpoko&line_height=40&show_icons=true)](https://github.com/anuraghazra/github-readme-stats)
+    I'm kinpoko!
 
-    ## Recent Posts on [My Blog](https://kinpokoblog.com)
+    Kaishain | Enjinia
+
+    ### Recent Posts on [My Blog](https://kinpokoblog.com)
     """)
 
     with open(GENERATE_FILE_NAME, 'w') as f:
